23ced6dde466164bea8bfa529ccd20a5676a6358
Matthew Lane <themplane@gmail.com>
2020-10-28 08:57:10 -0700

    Create POST endpoint.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const Game = require('./Game/Game.js');
+let game;
 
 app.set('port', process.env.PORT || 3000);
 
@@ -11,6 +13,19 @@ io.on('connection', (socket) => {
     console.log('A user has disconnected.', io.engine.clientsCount)
     io.sockets.emit(`User${io.engine.clientsCount} has disconnected`);
   })
+})
+
+app.post('/', (req, resp) => {
+  if (req.act === 'join') {
+    joinGame(resp);
+  }
+  if (req.act === 'word') {
+    startGame(req, resp)
+  }
+  if (req.act === 'guess') {
+    makeGuess(req, resp)
+  }
+  resp.status(400).json("Incorrect 'act' verb");
 })
 
 app.get('/', (req, resp) => {
